dd3e1e5df4624d78546ed9dd870b56dd411a301b
BvHand487 <84194378+BvHand487@users.noreply.github.com>
2023-06-04 19:06:42 +0300

    Delete encryption.h

--- encryption.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-char *generate_key(unsigned long);
-
-void __vigenere(char *, char *, char *, char);
-void encrypt_vigenere(char *, char *, char *);
-void decrypt_vigenere(char *, char *, char *);
