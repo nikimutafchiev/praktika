0ebd337ac8c7402f7a1ca9f254520d8872fc3214
Atefan <127405883+Atefan@users.noreply.github.com>
2023-06-13 20:09:48 +0300

    Update main.c
    Strcpy added and fixed

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include "cipher.h"
+#include <malloc.h>
 
 void create_new_user(char* name, char* password, const char* filename)
 {
@@ -30,7 +29,7 @@ void add_story(char* name, const char* user, const char* date, const char* story
 }
 struct stories
 {
-	char* name, * date, * user, * story;
+	char* title, * date, * user, * story;
 };
 struct stories_packet
 {
@@ -50,18 +49,22 @@ struct stories_packet* put_in_structs(const char* filename)
 	struct stories_packet* packet = malloc(sizeof(struct stories_packet));
 	packet->size = 0;
 	packet->buff = NULL;
-	char* name = malloc(sizeof(char) * 30);  // Replace MAX_LENGTH with the maximum expected length of the string
+	char* title = malloc(sizeof(char) * 100);  // Replace MAX_LENGTH with the maximum expected length of the string
 	char* user = malloc(sizeof(char) * 100);
-	char* date = malloc(sizeof(char) * 10);
+	char* date = malloc(sizeof(char) * 100);
 	char* story = malloc(sizeof(char) * 10000);
-	while (fscanf(file, "%[^\n]\n%[^\n]\n%[^\n]\n%[^\n]\n", user, date, name, story) == 4) {
+	while (fscanf(file, "%[^\n]\n%[^\n]\n%[^\n]\n%[^\n]\n", user, date, title, story) == 4) {
 		packet->size++;
 		packet->buff = realloc(packet->buff, packet->size * sizeof(struct stories*));
-		packet->buff[packet->size - 1] = malloc(sizeof(struct stories));
-		packet->buff[packet->size - 1]->date = date;
-		packet->buff[packet->size - 1]->user = user;
-		packet->buff[packet->size - 1]->name = name;
-		packet->buff[packet->size - 1]->story = story;
+		packet->buff[packet->size - 1] = malloc(sizeof(struct stories)); 
+		packet->buff[packet->size - 1]->title = malloc(sizeof(char) * 30);
+		packet->buff[packet->size - 1]->user = malloc(sizeof(char) * 100);
+		packet->buff[packet->size - 1]->date = malloc(sizeof(char) * 10);
+		packet->buff[packet->size - 1]->story = malloc(sizeof(char) * 10000);
+		strcpy(packet->buff[packet->size - 1]->date, date);
+		strcpy(packet->buff[packet->size - 1]->user, user);
+		strcpy(packet->buff[packet->size - 1]->title, title);
+		strcpy(packet->buff[packet->size - 1]->story, story);
 	}
 
 	fclose(file);
@@ -70,18 +73,11 @@ struct stories_packet* put_in_structs(const char* filename)
 
 int main()
 {
-	char *buffer;
-
-	buffer = generate_key(time(NULL), 64);
-
-	printf("%s\n", buffer);
-
-	/*create_new_user("Stefan Georgiev", "asdasdasd", "users.txt");
-	add_story("How i met your mother", "Josh", "29.2.2022", "uvu vue vue unicue veu veu u sans", "stories.txt");
-	add_story("How i met your mother", "Josh", "29.2.2022", "uvu vue vue unicue veu veu u sans", "stories.txt");
-	add_story("How i met your mother", "Josh", "29.2.2022", "uvu vue vue unicue veu veu u sans", "stories.txt");
-	add_story("How i met your mother", "Josh", "29.2.2022", "uvu vue vue unicue veu veu u sans", "stories.txt");
-	put_in_structs("stories.txt");*/
-
+	create_new_user("Stefan Georgiev", "asdasdasd", "users.txt");
+	add_story("How i met your mother1", "1", "29.2.2022", "1uvu vue vue unicue veu veu u sans", "stories.txt");
+	add_story("How i met your mother23", "2", "29.2.2022", "2uvu vue vue unicue veu veu u sans", "stories.txt");
+	add_story("How i met your mother3", "3", "29.2.2022", "3uvu vue vue unicue veu veu u sans", "stories.txt");
+	add_story("How i met your mother4", "4", "29.2.2022", "4uvu vue vue unicue veu veu u sans", "stories.txt");
+	put_in_structs("stories.txt");
 	return 0;
 }
