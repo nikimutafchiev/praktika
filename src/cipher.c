ad01d896eab30aefb93290b854d7b4453c9b6a9d
BvHand487 <84194378+BvHand487@users.noreply.github.com>
2023-06-14 19:20:18 +0300

    Update cipher.c

--- src/cipher.c
+++ src/cipher.c
@@ -1,41 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "cipher.h"
 
-char *generate_key(unsigned int seed, size_t size)
+void init(const char *iv)
 {
-	srand(seed);
+	cipher.history = malloc(sizeof(char *));
 
-	char *key = (char *) malloc(size + 1);
+	cipher.len = 1;
+
+	cipher.history[0] = malloc(MAX_VECTOR + 1);
+	strcpy(cipher.history[0], iv);
+}
+
+void push(char *iv)
+{
+	cipher.history = realloc(cipher.history, sizeof(char *) * ++cipher.len);
+
+	cipher.history[cipher.len - 1] = malloc(MAX_VECTOR + 1);
+	strcpy(cipher.history[cipher.len - 1], iv);
+}
+
+void pop(void)
+{
+	if (cipher.len == 1)
+		return;
+
+	free(cipher.history[cipher.len - 1]);
+
+	cipher.history = realloc(cipher.history, sizeof(char *) * --cipher.len);
+}
+
+void xor(char *data, const char *vector, size_t data_len)
+{
 	int i = 0;
 
-	for (; i < size; ++i)
-		key[i] = rand() % 26 + 'a';
-
-	key[i] = 0;
-
-	return key;
-}
-
-void __vigenere(unsigned char *input, unsigned char *key, unsigned char *output, unsigned char operation)
-{
-	unsigned char *key_start = key;
-
-	while (*input)
+	for (; i < data_len && data[i]; ++i)
 	{
-		if (operation)
-			continue;
-		else
-			continue;
+		data[i] ^= vector[i % MAX_VECTOR];
 	}
 
-	*output = 0;
+	data[i] = 0;
 }
 
-void encrypt_vigenere(unsigned char *input, unsigned char *key, unsigned char *output)
+void __encrypt(char *data, const char *key, size_t data_len)
 {
-	__vigenere(input, key, output, (unsigned char) ENCRYPT);
+	size_t key_len = strlen(key);
+	int i = 0;
+
+	for (; i < data_len && data[i]; ++i)
+	{
+		data[i] += key[i % key_len];
+	}
+
+	data[i] = 0;
 }
 
-void decrypt_vigenere(unsigned char *input, unsigned char *key, unsigned char *output)
+void __decrypt(char *data, const char *key, size_t data_len)
 {
-	__vigenere(input, key, output, (unsigned char) DECRYPT);
+	size_t key_len = strlen(key);
+	int i = 0;
+
+	for (; i < data_len && data[i]; ++i)
+	{
+		data[i] -= key[i % key_len];
+	}
+
+	data[i] = 0;
+}
+
+void encrypt(char *data, const char *key, size_t data_len)
+{
+	xor(data, cipher.history[cipher.len - 1], data_len);
+
+	__encrypt(data, key, data_len);
+
+	push(data, data_len);
+}
+
+void decrypt(char *data, const char *key, size_t data_len)
+{
+	if (cipher.len == 1)
+		return;
+
+	__decrypt(data, key, data_len);
+
+	xor(data, cipher.history[cipher.len - 2], data_len);
+
+	pop();
+}
+
+int main()
+{
+	init("INITIAL_VECTOR_");
+
+	char buffer[256] = "Womp womp!";
+	char b[256] = "Hello world";
+
+	encrypt(buffer, "gotin key 123", sizeof buffer);
+	encrypt(b, "blah blah", sizeof b);
+
+	decrypt(b, "blah blah", sizeof b);
+	decrypt(buffer, "gotin key 123", sizeof buffer);
+
+	printf("%s\n", b);
+	printf("%s\n", buffer);
+
+	return 0;
 }
