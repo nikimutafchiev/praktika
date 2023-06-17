c2123c73bc4327b7cb81cda46b2fbd53018a64f0
BvHand487 <84194378+BvHand487@users.noreply.github.com>
2023-06-17 14:36:28 +0300

    Update cipher.h

--- src/cipher.h
+++ src/cipher.h
@@ -3,21 +3,22 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define MAX_VECTOR 16
 
-void init(const char *);
-void push(char *);
-void pop(void);
-void xor(char *, const char *, size_t);
-void __encrypt(char *, const char *, size_t);
-void __decrypt(char *, const char *, size_t);
-void encrypt(char *, const char *, size_t);
-void decrypt(char *, const char *, size_t);
-
-struct cipher_t
+typedef struct cipher_t
 {
 	char **history;
 	size_t len;
 
-} cipher;
+} Cipher;
+
+Cipher* init(const char *);
+void push(Cipher *, char *);
+void pop(Cipher *);
+void xor(char *, const char *, size_t);
+void __encrypt(char *, const char *, size_t);
+void __decrypt(char *, const char *, size_t);
+void encrypt(Cipher *, char *, const char *, size_t);
+void decrypt(Cipher *, char *, const char *, size_t);
